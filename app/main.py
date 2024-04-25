8a88292615fc9eaccf77aaddb8fd8aac5011917f
Yeabsira Tibebu <61262880+YeabsiraTibebu@users.noreply.github.com>
2024-04-25 20:10:13 +0300

    Update main.py
    Create a dashboard with a sidebar for uploading a data

--- app/main.py
+++ app/main.py
@@ -3,3 +3,23 @@ import pandas as pd
 import plotly.express as px
 
 st.title("Dashboard")
+
+def load_data(path:str):
+    data = pd.read_csv(path)
+    return data
+
+with st.sidebar:
+    upload_file = st.file_uploader("Choose a file", type=["csv", "xlsx",'pdf'])
+    
+    if upload_file is None:
+        st.info("upload file through config",icon="ℹ️")
+        st.stop()
+ 
+        
+if upload_file is not None:
+    data = load_data(upload_file)
+    st.dataframe(data)
+
+
+
+
